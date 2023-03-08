d5e3be616616fff2030a448a048195fd4182ebc9
Riteek Khaul <rkhaul2000@gmail.com>
2023-03-08 13:10:24 +0530

    date format changed

--- src/components/Certificate.jsx
+++ src/components/Certificate.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import '../App.css';
 
 function Certificate({certData}){
+
+
   return (
     <>
     <div id="certcon" className="w-800 h-600 mt-4 mb-20 mx-auto bg-white border border-black text-center box-border">
@@ -18,8 +20,8 @@ function Certificate({certData}){
       <div className="text-xl font-bold mb-12">Certificate ID : {certData._id}</div>
       <p className="mx-8 mb-4"> txn no. : {certData.cert_txn}</p>
       <div className="text-lg italic flex">
-      <p className="ml-8 mb-4"> Issued on : {certData.date.substr(0,10)} </p>
-      <p className="ml-52 mb-4">Company : {certData.companyName}</p>
+      <p className="ml-4 mb-4"> Issued on : {certData.date} </p>
+      <p className="ml-44 mb-4 mr-4">Company : {certData.companyName}</p>
       </div>
      </div>
       </>
